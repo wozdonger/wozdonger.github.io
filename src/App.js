a68e64c29d62d800a9889de27c264968e19166c5
wozdonger <wozdonger@gmail.com>
2021-01-11 14:34:13 +0700

    home

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>WOZDONGER</h1>
+      <p>Hello this is homepage.</p>
+    </div>
+  );
+}
+
+export default App;
